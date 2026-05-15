fb9dea31149fa7c5ee6ee11d63eb04ea0e356919
SwipeDexPWA <swipedex.app@gmail.com>
2026-05-16 07:17:13 +0800

    Add files via upload

---
+++ joelgallinero/data.nJect.js
@@ -0,0 +1,33 @@
+const data = {
+
+    firstname: "Joel",
+    middlename: "L.",
+    lastname: "Gallinero",
+    companyname: "BrushNetwork",
+    companydescription: "a Graphic Design & Web Development company since 1997",
+
+    messenger: "m.me/joelgallinero",
+    mobile: "09164729358",
+    email: "joelgallinero@gmail.com",
+    website: "www.brushnetwork.com",
+
+    linkedin: "",
+    x: "",
+    facebook: "",
+    instagram: "",
+
+    slideshow1: '<div><img src="slideshow1.webp" alt="" uk-cover><div class="uk-position-bottom-left uk-position-large"><h2 class="montserrat-black box-heading" uk-slideshow-parallax="y: 100, 0, 100">Heading</h2><p class="poppins-regular box-paragraph" uk-slideshow-parallax="y: 200, 0, 200">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p></div></div>',
+    slideshow2: '<div><img src="slideshow2.webp" alt="" uk-cover><div class="uk-position-bottom-left uk-position-large"><h2 class="montserrat-black box-heading" uk-slideshow-parallax="y: 100, 0, 100">Heading</h2><p class="poppins-regular box-paragraph" uk-slideshow-parallax="y: 200, 0, 200">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p></div></div>',
+    slideshow3: '<div><img src="slideshow3.webp" alt="" uk-cover><div class="uk-position-bottom-left uk-position-large"><h2 class="montserrat-black box-heading" uk-slideshow-parallax="y: 100, 0, 100">Heading</h2><p class="poppins-regular box-paragraph" uk-slideshow-parallax="y: 200, 0, 200">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p></div></div>',
+    slideshow4: '<div><img src="slideshow4.webp" alt="" uk-cover><div class="uk-position-bottom-left uk-position-large"><h2 class="montserrat-black box-heading" uk-slideshow-parallax="y: 100, 0, 100">Heading</h2><p class="poppins-regular box-paragraph" uk-slideshow-parallax="y: 200, 0, 200">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p></div></div>',
+    slideshow5: '<div><img src="slideshow5.webp" alt="" uk-cover><div class="uk-position-bottom-left uk-position-large"><h2 class="montserrat-black box-heading" uk-slideshow-parallax="y: 100, 0, 100">Heading</h2><p class="poppins-regular box-paragraph" uk-slideshow-parallax="y: 200, 0, 200">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p></div></div>',
+    slideshow6: '<div><img src="slideshow6.webp" alt="" uk-cover><div class="uk-position-bottom-left uk-position-large"><h2 class="montserrat-black box-heading" uk-slideshow-parallax="y: 100, 0, 100">Heading</h2><p class="poppins-regular box-paragraph" uk-slideshow-parallax="y: 200, 0, 200">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p></div></div>',
+
+    grid1: '<div class="uk-cover-container"><a href="grid1.webp" data-caption="Tile1"><img src="grid1.webp" uk-cover></a></div>',
+    grid2: '<div class="uk-cover-container"><a href="grid2.webp" data-caption="Tile2"><img src="grid2.webp" uk-cover></a></div>',
+    grid3: '<div class="uk-cover-container"><a href="grid3.webp" data-caption="Tile3"><img src="grid3.webp" uk-cover></a></div>',
+    grid4: '<div class="uk-cover-container"><a href="grid4.webp" data-caption="Tile4"><img src="grid4.webp" uk-cover></a></div>',
+    grid5: '<div class="uk-cover-container"><a href="grid5.webp" data-caption="Tile5"><img src="grid5.webp" uk-cover></a></div>',
+    grid6: '<div class="uk-cover-container"><a href="grid6.webp" data-caption="Tile6"><img src="grid6.webp" uk-cover></a></div>'
+
+};
