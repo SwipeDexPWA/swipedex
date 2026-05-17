0369de13168d0d22290f97ca7b33253fe108ba0a
SwipeDexPWA <swipedex.app@gmail.com>
2026-05-17 11:43:49 +0800

    Add files via upload

--- grid.v1.3x2.Section.js
+++ grid.v1.3x2.Section.js
@@ -1,7 +1,7 @@
 document.write(`
 <div class="uk-section uk-padding-remove-vertical">
     <div class="uk-container uk-container-expand uk-padding-remove-horizontal uk-margin-remove">
-        <div id="grids" class="uk-child-width-1-3@m uk-child-width-1-2@s uk-grid-collapse uk-height-viewport"
+        <div id="grids" class="uk-child-width-1-3@s uk-child-width-1-2 uk-grid-collapse uk-height-viewport"
             uk-grid uk-lightbox="animation: slide; counter: true;" uk-height-match="row: false; target: > img">
                 {{grid1}}
                 {{grid2}}
