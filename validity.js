85442362fa064e4a4840ee53374bcf17fb0fe7da
SwipeDexPWA <swipedex.app@gmail.com>
2026-07-10 09:49:38 +0800

    Update validity.js

--- validity.js
+++ validity.js
@@ -4,21 +4,16 @@ let redirectURL = null;
 fetch("data.json")
     .then(r => r.json())
     .then(data => {
-        license = data.license;
-        expireTime = new Date(license.expires).getTime();
-        redirectURL = license.redirect;
-        if (license.disabled) {
-            lock();
-            return;
-        }
-        if (Date.now() >= expireTime) {
-            lock();
-            return;
-        }
-        const app = document.getElementById("pwa");
-        if (app) app.style.display = "block";
-        startCountdown();
-    })
+
+    console.log("DATA:", data);
+
+    if (!data.license) {
+        console.error("No license object!");
+        document.getElementById("pwa").style.display = "block";
+        return;
+    }
+
+    license = data.license;)
     .catch(err => {
         console.error("License error:", err);
     });
