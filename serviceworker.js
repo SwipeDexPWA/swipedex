ee4119e017439891d161c36585d842247cbf9ed1
SwipeDexPWA <swipedex.app@gmail.com>
2026-07-10 08:05:14 +0800

    Update serviceworker.js

--- serviceworker.js
+++ serviceworker.js
@@ -1,13 +1,34 @@
-const CACHE_NAME = "swipedex-v2";
+const CACHE_NAME = "swipedex-test-v1";
+
 
 self.addEventListener("install", event => {
+
+    console.log("SW INSTALL");
+
+    event.waitUntil(
+
+        caches.open(CACHE_NAME)
+        .then(cache => {
+
+            console.log("Creating cache");
+
+            return cache.add(
+                "/cupofjoe/"
+            );
+
+        })
+
+    );
 
     self.skipWaiting();
 
 });
 
 
+
 self.addEventListener("activate", event => {
+
+    console.log("SW ACTIVE");
 
     event.waitUntil(
         self.clients.claim()
@@ -16,75 +37,52 @@ self.addEventListener("activate", event => {
 });
 
 
+
 self.addEventListener("fetch", event => {
-
-
-    if (event.request.method !== "GET") {
-        return;
-    }
-
-
-    const requestURL = new URL(event.request.url);
-
-
-    // Only cache SwipeDex pages
-    if (
-        requestURL.hostname !== "swipedex.app" &&
-        requestURL.hostname !== "www.swipedex.app"
-    ) {
-        return;
-    }
-
-
 
     event.respondWith(
 
         caches.match(event.request)
 
-        .then(cached => {
+        .then(response => {
 
+            if (response) {
 
-            if (cached) {
-                return cached;
+                console.log(
+                    "CACHE HIT:",
+                    event.request.url
+                );
+
+                return response;
+
             }
 
 
-
             return fetch(event.request)
-
-            .then(response => {
-
-
-                if (
-                    !response ||
-                    response.status !== 200
-                ) {
-                    return response;
-                }
+            .then(networkResponse => {
 
 
-                const copy =
-                    response.clone();
-
+                const clone =
+                    networkResponse.clone();
 
 
                 caches.open(CACHE_NAME)
-
                 .then(cache => {
 
+                    console.log(
+                        "Caching:",
+                        event.request.url
+                    );
 
                     cache.put(
                         event.request,
-                        copy
+                        clone
                     );
-
 
                 });
 
 
-
-                return response;
-
+                return networkResponse;
 
             });
 
@@ -92,6 +90,5 @@ self.addEventListener("fetch", event => {
         })
 
     );
-
 
 });
