ad051aa3d65d9a8c42df8356c7efc78c08eeb93a
SwipeDexPWA <swipedex.app@gmail.com>
2026-07-10 09:56:43 +0800

    Update validity.js

--- validity.js
+++ validity.js
@@ -1,30 +1,88 @@
 let license = null;
 let expireTime = null;
 let redirectURL = null;
+
 fetch("data.json")
     .then(r => r.json())
     .then(data => {
 
-    console.log("DATA:", data);
+        console.log("DATA:", data);
 
-    if (!data.license) {
-        console.error("No license object!");
-        document.getElementById("pwa").style.display = "block";
-        return;
-    }
+        if (!data.license) {
+            console.error("No license object!");
 
-    license = data.license;)
+            const app = document.getElementById("pwa");
+
+            if (app) {
+                app.style.display = "block";
+            }
+
+            return;
+        }
+
+        license = data.license;
+
+        expireTime = new Date(license.expires).getTime();
+        redirectURL = license.redirect;
+
+
+        if (license.disabled) {
+            lock();
+            return;
+        }
+
+
+        if (Date.now() >= expireTime) {
+            lock();
+            return;
+        }
+
+
+        const app = document.getElementById("pwa");
+
+        if (app) {
+            app.style.display = "block";
+        }
+
+
+        startCountdown();
+
+    })
     .catch(err => {
+
         console.error("License error:", err);
-    };
+
+        const app = document.getElementById("pwa");
+
+        if (app) {
+            app.style.display = "block";
+        }
+
+    });
+
+
+
 function lock() {
+
     const app = document.getElementById("pwa");
-    if (app) app.style.display = "none";
-    UIkit.modal("#license").show();
-    const btn = document.getElementById("provider");
-    if (btn) {
-        btn.onclick = () => {
-            window.location.href = redirectURL;
-        };
+
+    if (app) {
+        app.style.display = "none";
     }
+
+    UIkit.modal("#license").show();
+
+
+    const btn = document.getElementById("provider");
+
+    if (btn) {
+
+        btn.onclick = () => {
+
+            window.location.href = redirectURL;
+
+        };
+
+    }
+
 }
