59cde5da24350e12e5e2082db0b32d5c11eeef37
SwipeDexPWA <swipedex.app@gmail.com>
2026-07-10 10:43:16 +0800

    Update pwa.js

--- pwa.js
+++ pwa.js
@@ -1,30 +1,23 @@
 window.addEventListener("load", () => {
     registerSW();
 });
-
 async function registerSW() {
-
     const allowedHosts = [
         "swipedex.app",
         "www.swipedex.app"
     ];
-
     const isMobileOrTablet =
         window.innerWidth <= 1024 ||
         ("ontouchstart" in window) ||
         navigator.maxTouchPoints > 0;
-
     if (
         !allowedHosts.includes(location.hostname) ||
         !isMobileOrTablet
     ) {
         return;
     }
-
     if ("serviceWorker" in navigator) {
-
         try {
-
             const registration =
                 await navigator.serviceWorker.register(
                     "/serviceworker.js",
@@ -32,21 +25,15 @@ async function registerSW() {
                         scope: "./"
                     }
                 );
-
             console.log(
                 "SW registered:",
                 registration.scope
             );
-
         } catch (error) {
-
             console.error(
                 "Service Worker registration failed:",
                 error
             );
-
         }
-
     }
-
 }
