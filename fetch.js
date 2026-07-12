bfe599dd8310500ae9ae6729c3bec830148b0cc9
SwipeDexPWA <swipedex.app@gmail.com>
2026-07-13 07:24:28 +0800

    Add files via upload

--- fetch.js
+++ fetch.js
@@ -1,13 +1,15 @@
-(function initGlobalProtection() {
-    const allowedHosts = ["swipedex.app", "www.swipedex.app"];
-    const isMobileOrTablet = ("ontouchstart" in window) || (navigator.maxTouchPoints > 0);
-    if (!allowedHosts.includes(window.location.hostname) || !isMobileOrTablet) {
-        document.body.innerHTML = "<h1>Unauthorized Environment</h1>";
-        throw new Error("PWA assets locked.");
-    }
-})();
 window.addEventListener("load", registerSW);
 async function registerSW() {
+    const allowedHosts = [
+        "swipedex.app",
+        "www.swipedex.app"
+    ];
+    const isMobileOrTablet =
+        ("ontouchstart" in window) ||
+        (navigator.maxTouchPoints > 0);
+    if (!allowedHosts.includes(location.hostname) || !isMobileOrTablet) {
+        return;
+    }
     if ("serviceWorker" in navigator) {
         try {
             await navigator.serviceWorker.register("/serviceworker.js", {
