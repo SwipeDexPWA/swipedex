24d97fe0dd418c68d2f5f81cc014d520089c56fc
SwipeDexPWA <swipedex.app@gmail.com>
2026-08-16 08:58:16 +0800

    Update fetch.js

--- fetch.js
+++ fetch.js
@@ -12,7 +12,7 @@ async function registerSW() {
     }
     if ('serviceWorker' in navigator) {
         window.addEventListener('load', () => {
-            navigator.serviceWorker.register('./serviceworker.js')
+            navigator.serviceWorker.register('/serviceworker.js')
                 .then(registration => {
                     console.log(
                         'Service Worker registered:',
