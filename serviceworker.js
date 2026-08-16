e392dc15527077b6a2b79f2bba07884ea209040c
SwipeDexPWA <swipedex.app@gmail.com>
2026-08-16 17:25:47 +0800

    Update serviceworker.js

--- serviceworker.js
+++ serviceworker.js
@@ -1,23 +1,17 @@
-const allowedHosts = ["swipedex.app", "www.swipedex.app"];
-const isMobileOrTablet =
-    ("ontouchstart" in self) ||
-    (self.navigator && self.navigator.maxTouchPoints > 0);
-const isAllowedHost = allowedHosts.includes(self.location.hostname);
-if (!isAllowedHost || !isMobileOrTablet) {
-} else {
-    var staticCacheName = "pwa";
-    self.addEventListener("install", function (e) {
-        e.waitUntil(
-            caches.open(staticCacheName).then(function (cache) {
-                return cache.addAll(["/"]);
-            })
-        );
-    });
-    self.addEventListener("fetch", function (event) {
-        event.respondWith(
-            caches.match(event.request).then(function (response) {
-                return response || fetch(event.request);
-            })
-        );
-    });
-}
+var staticCacheName = "pwa";
+self.addEventListener("install", function (e) {
+    e.waitUntil(
+        caches.open(staticCacheName).then(function (cache) {
+            return cache.addAll(["/"]);
+        })
+    );
+});
+self.addEventListener("fetch", function (event) {
+    console.log(event.request.url);
+
+    event.respondWith(
+        caches.match(event.request).then(function (response) {
+            return response || fetch(event.request);
+        })
+    );
+});
