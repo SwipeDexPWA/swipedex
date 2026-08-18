ce6d0284b307ea2eac5f1dd9cb98b05f7e13ca93
SwipeDexPWA <swipedex.app@gmail.com>
2026-08-19 03:51:31 +0800

    Add files via upload

---
+++ anigen.js
@@ -0,0 +1,12 @@
+// Slideshow
+UIkit.slideshow('#slideshow #player', { autoplay: 'true', autoplayInterval: '7000', animation: 'push', ratio: 'false' });
+UIkit.slideshowParallax('#slideshow #animation1', { y: '100, 0, 100' });
+UIkit.slideshowParallax('#slideshow #animation2', { y: '200, 0, 200' });
+UIkit.slideshowParallax('#slideshow #animation3', { y: '300, 0, 300' });
+
+// About
+document.getElementById("js-sticky-parallax-container").classList.add("uk-height-viewport-3");
+UIkit.parallax('#about #background', { bgy: '-300' });
+UIkit.heightViewport('#about #background', {});
+UIkit.parallax('#about #animation1', { target: '#js-sticky-parallax-container', x: '-15vw,0', easing: -1, start: '80vh', end: '100vh' });
+UIkit.parallax('#about #animation2', { target: '#js-sticky-parallax-container', x: '10vw,0', easing: -1, start: '70vh', end: '100vh' });
