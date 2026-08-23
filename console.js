bf52b2553740c3d5b381f039870df739efc2cf35
SwipeDexPWA <swipedex.app@gmail.com>
2026-08-24 07:23:33 +0800

    Add files via upload

--- console.js
+++ console.js
@@ -1,15 +1,22 @@
-
 document.addEventListener("click", (e) => {
+
     const el = e.target.closest(".qr-trigger");
+
     if (!el) return;
+
     const title = el.dataset.title;
     const qr = el.dataset.qr;
     const key = el.dataset.key;
     let link = el.dataset.link || "";
+    const location = el.dataset.location || "";
+
     document.getElementById("qr-title").textContent = title;
     document.getElementById("qr-image").src = qr;
+
     const linkEl = document.getElementById("qr-link");
+
     switch (key) {
+
         case "email":
             link = `mailto:${link}`;
             break;
@@ -23,12 +30,24 @@ document.addEventListener("click", (e) => {
         case "sms":
             link = `sms:${link}`;
             break;
+
     }
+
     if (link) {
+
         linkEl.href = link;
-        linkEl.textContent = el.dataset.link;
+
+        linkEl.textContent =
+            ["googlemaps", "waze", "openstreetmap"].includes(key)
+                ? location
+                : el.dataset.link;
+
         linkEl.style.display = "block";
+
     } else {
+
         linkEl.style.display = "none";
+
     }
+
 });
