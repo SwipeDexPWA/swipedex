c2b1dcce79a99f04b36cde65ffd38e438b802209
SwipeDexPWA <swipedex.app@gmail.com>
2026-08-26 07:47:56 +0800

    Add files via upload

--- price.js
+++ price.js
@@ -1,8 +1,5 @@
 const prices = {
   PH: {
-    basic: '₱500<small>/Year</small>',
-    standard: '₱750<small>/Year</small>',
-    premium: '₱1000<small>/Year</small>',
     pwa: '₱500',
     opw: '₱500',
     add: '₱250',
@@ -10,9 +7,6 @@ const prices = {
     rev: '₱125'
   },
   INTL: {
-    basic: '$15<small>/Year</small>',
-    standard: '$25<small>/Year</small>',
-    premium: '$35<small>/Year</small>',
     pwa: '$15',
     opw: '$15',
     add: '$10',
@@ -22,9 +16,6 @@ const prices = {
 };
 const timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
 const region = timezone === 'Asia/Manila' ? 'PH' : 'INTL';
-document.getElementById('basic-price').innerHTML = prices[region].basic;
-document.getElementById('standard-price').innerHTML = prices[region].standard;
-document.getElementById('premium-price').innerHTML = prices[region].premium;
 document.getElementById('pwa-price').innerHTML = prices[region].pwa;
 document.getElementById('opw-price').innerHTML = prices[region].opw;
 document.getElementById('add-price').innerHTML = prices[region].add;
